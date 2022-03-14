de97b0f137f7736e2a0d9c160110f9867ecbc1dc
Michael Schneider <schneidm@goldmail.etsu.edu>
2022-03-14 16:39:29 -0600

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -67,9 +67,9 @@ namespace CircuitCheck {
     export function pause(timePaused: number) {
         basic.showIcon(IconNames.Heart);
         //basic.pause(timePaused);
-        let localTimer = input.runningTime() + timePaused;
+        let localTimer = control.millis() + timePaused;
         //variable_transmitter();//Send current state of variables
-        while(localTimer < input.runningTime()){
+        while(true){
             /*if (timer + delay < input.runningTime()) {
                 sendScreenshot();//Send current state of LED matrix, so that CC can mirror it
                 checkMessages("Circuit Check Running inside Pause");
